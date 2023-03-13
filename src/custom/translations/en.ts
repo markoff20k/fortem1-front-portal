ae134a3c13c2e0f575c401148099e563551621ed
Jeferson Martin de Araujo <jmartin20k@gmail.com>
2023-03-13 06:28:13 -0300

    v3

--- src/custom/translations/en.ts
+++ src/custom/translations/en.ts
@@ -1,7 +1,7 @@
 export const en = {
 
 
-    'page.header.signIn.title': 'Login with your email used in SignUp',
+    'page.header.signIn.title': 'Login with your email used in signup',
     'page.header.signUp.title': 'Start your investment journey',
 
     'page.header.signUp.username': 'What is your nickname ?',
